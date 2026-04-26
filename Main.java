6d9c79a83e474ed9bcd1213cd1d717a73e215bf2
Naren B <bnaren18@gmail.com>
2026-04-26 21:00:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        //Enhanced switch=A replacement to many else if statements
+        //                 (Java14 feature)
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a day of the week: ");
+        String day=scanner.nextLine();
+
+
+        switch(day){
+            case "Monday" -> System.out.println("It is a weekday 😒");
+            case "Tuesday" -> System.out.println("It is a weekday 😒");
+            case "Wednesday" -> System.out.println("It is a weekday 😒");
+            case "Thursday" -> System.out.println("It is a weekday 😒");
+            case "Friday" -> System.out.println("It is a weekday 😒");
+            case "Saturday" -> System.out.println("It is a weekend 🥳");
+            case "Sunday" -> System.out.println("It is a weekend 🥳");
+            
+            default -> System.out.println(day+" is not a day of the week");
+
+            
+        }
+        scanner.close();
+    }
+}
